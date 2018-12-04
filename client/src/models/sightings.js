7fab67f9f5c9a6fa5ce44acbd3542cc16fd827f9
Ernest Fraiman <upapaus@gmail.com>
2018-12-04 15:47:16 +0000

    lab done

---
+++ client/src/models/sightings.js
@@ -0,0 +1,45 @@
+const RequestHelper = require('../helpers/request_helper.js');
+const PubSub = require('../helpers/pub_sub.js');
+
+const Sightings = function (url) {
+  this.url = url;
+  this.request = new RequestHelper(this.url);
+};
+
+Sightings.prototype.bindEvents = function () {
+  PubSub.subscribe('SightingView:sighting-delete-clicked', (evt) => {
+    this.deleteSighting(evt.detail);
+  });
+
+  PubSub.subscribe('SightingFormView: sighting-submitted', (evt) => {
+    this.createSighting(evt.detail);
+    console.log(evt.detail);
+  })
+};
+
+Sightings.prototype.getData = function () {
+  this.request.get()
+    .then((sightings) => {
+      PubSub.publish('Sightings:data-loaded', sightings);
+    })
+    .catch(console.error);
+};
+
+Sightings.prototype.deleteSighting = function (sightingId) {
+  this.request.delete(sightingId)
+    .then((sightings) => {
+      PubSub.publish('Sightings:data-loaded', sightings);
+    })
+    .catch(console.error);
+};
+
+Sightings.prototype.createSighting = function (sighting) {
+  //const request = new RequestHelper(this.url);
+  this.request.post(sighting)
+  .then((sightings) => {
+    PubSub.publish('Sightings:data-loaded', sightings);
+  })
+  .catch(console.error);
+};
+
+module.exports = Sightings;
